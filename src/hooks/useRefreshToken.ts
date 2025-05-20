b4dadddb275ef9a507c00cd0f132f2ab6d3313f9
codingdud <codingdud@gmail.com>
2025-05-21 02:06:00 +0530

    fix:api query

--- src/hooks/useRefreshToken.ts
+++ src/hooks/useRefreshToken.ts
@@ -7,7 +7,7 @@ const useRefreshToken = () => {
 
   const refresh = async () => {
     try {
-      const refreshToken = localStorage.getItem('refreshToken');
+      const refreshToken = localStorage.getItem('rtoken');
       const response = await axios.post('/api/v1/auth/refresh-token',refreshToken, {
         withCredentials: true,
       });
